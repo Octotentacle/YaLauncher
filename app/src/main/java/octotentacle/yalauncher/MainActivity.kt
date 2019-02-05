b85d505e22fc982812639b7c691aa904cdad74ac
Andrei Khilko <andrekhilko@gmail.com>
2019-02-05 18:41:40 +0300

    test

--- app/src/main/java/octotentacle/yalauncher/MainActivity.kt
+++ app/src/main/java/octotentacle/yalauncher/MainActivity.kt
@@ -7,7 +7,6 @@ import io.fabric.sdk.android.Fabric
 import com.microsoft.appcenter.AppCenter
 import com.microsoft.appcenter.analytics.Analytics
 import com.microsoft.appcenter.crashes.Crashes
-import com.microsoft.appcenter.push.Push
 
 
 class MainActivity : AppCompatActivity() {
@@ -16,8 +15,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         AppCenter.start(
             application, "78320121-e28f-4703-a7f1-c8da1bb912ab",
-            Analytics::class.java, Crashes::class.java, Push::class.java
-        )
+            Analytics::class.java, Crashes::class.java)
         Fabric.with(this, Crashlytics())
         setContentView(R.layout.activity_main)
     }
