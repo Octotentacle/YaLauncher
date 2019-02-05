7c642d0872786084c97e309db25ab82ae96c876c
Andrei Khilko <andrekhilko@gmail.com>
2019-02-05 18:24:23 +0300

    test

--- app/src/main/java/octotentacle/yalauncher/MainActivity.kt
+++ app/src/main/java/octotentacle/yalauncher/MainActivity.kt
@@ -4,13 +4,20 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.crashlytics.android.Crashlytics
 import io.fabric.sdk.android.Fabric
-
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+import com.microsoft.appcenter.push.Push
 
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppCenter.start(
+            application, "78320121-e28f-4703-a7f1-c8da1bb912ab",
+            Analytics::class.java, Crashes::class.java, Push::class.java
+        )
         Fabric.with(this, Crashlytics())
         setContentView(R.layout.activity_main)
     }
